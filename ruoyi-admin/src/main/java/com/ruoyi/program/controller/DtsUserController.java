53d4a56e66776e4ef22c25abe6ba3049e6fbac7e
薛强 <2741859110@qq.com>
2024-06-27 16:23:39 +0800

    创建用户管理 新增，分页查询 新增注释

--- ruoyi-admin/src/main/java/com/ruoyi/program/controller/DtsUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/program/controller/DtsUserController.java
@@ -42,17 +42,40 @@ public class DtsUserController {
         }
     }
 
+    /**
+     * 分页查询用户信息。
+     * 通过此接口可以实现用户信息的分页查询，支持根据用户条件进行筛选。
+     * 使用PageHelper进行分页处理，返回分页后的用户列表及总条数。
+     *
+     * @param dtsUser  查询条件对象，包含用户信息的过滤条件。
+     * @param pageNum  当前页码，默认为1。
+     * @param pageSize 每页显示的记录数，默认为10。
+     * @return 返回一个Map对象，包含用户列表和总记录数。
+     * "data" 键存储总记录数，"list" 键存储用户列表。
+     */
     @ApiOperation(value = "分页查询用户")
     @GetMapping("/selectDtsUser")
     public Map<String, Object> selectDtsUser(DtsUser dtsUser,
                                              @RequestParam(defaultValue = "1") Integer pageNum,
                                              @RequestParam(defaultValue = "10") Integer pageSize) {
+        // 初始化返回数据的Map对象
         HashMap<String, Object> map = new HashMap<>();
+
+        // 开始分页查询，设置当前页码和每页记录数
         PageHelper.startPage(pageNum, pageSize);
+
+        // 根据查询条件查询所有用户信息，限制返回的结果集大小
         List<DtsUser> list = dtsUserMapper.queryAllByLimit(dtsUser);
+
+        // 创建PageInfo对象，用于包装分页信息
         PageInfo<DtsUser> info = new PageInfo<>(list);
+
+        // 将总记录数和用户列表分别放入map中
         map.put("data", info.getTotal());
         map.put("list", list);
+
+        // 返回包含分页信息的Map对象
         return map;
     }
+
 }

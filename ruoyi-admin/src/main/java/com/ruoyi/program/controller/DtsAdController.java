1c700c98bf3a9b16df3461ec9eec02564ad3be31
薛强 <2741859110@qq.com>
2024-06-29 09:25:38 +0800

    对上传图片进行优化

--- ruoyi-admin/src/main/java/com/ruoyi/program/controller/DtsAdController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/program/controller/DtsAdController.java
@@ -7,25 +7,18 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Api(tags = {"广告管理"})
 @RestController
